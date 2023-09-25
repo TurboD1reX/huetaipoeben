4f8288163b2a6f52c77c49c7cf9b37ffd24fd2c9
Yehor Kryvoi <126288081+TurboD1reX@users.noreply.github.com>
2023-09-25 21:23:16 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -12,13 +12,16 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0; 
 }
+
+
+
 var artists = document.querySelector('.artists');
-var partnersContainer = document.querySelector('.partners-container');
+var  newsContainer= document.querySelector('.news-container');
 var socialMedia = document.querySelector('.social-media');
 var footer = document.querySelector('footer');
 socialMedia.addEventListener('click', function() {
     footer.scrollIntoView({ behavior: 'smooth' });
 });
 artists.addEventListener('click', function() {
-    partnersContainer.scrollIntoView({ behavior: 'smooth' });
-});
+    newsContainer.scrollIntoView({ behavior: 'smooth' });
+})
