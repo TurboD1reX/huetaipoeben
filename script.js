b61e6669557c6506c881a47be3ece78ec381d9b9
Yehor Kryvoi <126288081+TurboD1reX@users.noreply.github.com>
2023-10-03 23:28:57 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,28 +5,36 @@ function scrollFunction() {
     if (document.body.scrollTop > 220 || document.documentElement.scrollTop > 220) {
     scrollBtn.style.display = "block";
 } else {
+    scrollBtn.style.bottom = "-60px";
     scrollBtn.style.display = "none";
 }
 }
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0; 
+
 }
 
 
 
-var artists = document.querySelector('.artists');
-var downButton = document.getElementById('buttonDownNews')
-var  NewsKsmLabel= document.querySelector('.ksm-label');
-var socialMedia = document.querySelector('.social-media');
-var footer = document.querySelector('footer');
+var NewsLink = document.querySelector('.news-link');
+var downButton = document.getElementById('buttonDownNews');
+var NewsKsmLabel = document.querySelector('.ksm-label');
 var IndexNewsContainer = document.querySelector('.news-container');
-socialMedia.addEventListener('click', function() {
-    footer.scrollIntoView({ behavior: 'smooth' });
-});
+
 downButton.addEventListener('click', function() {
     NewsKsmLabel.scrollIntoView({ behavior: 'smooth' });
-})
-artists.addEventListener('click', function() {
+});
+
+NewsLink.addEventListener('click', function() {
     IndexNewsContainer.scrollIntoView({ behavior: 'smooth' });
-})
+});
+
+function socialMedia(){
+var titleNews = document.querySelector('.title');
+var MediaContainer = document.querySelector('.social-media-container');
+var PreorderContainer = document.getElementById('container_preorder');
+titleNews.style.display =  "none";
+MediaContainer.style.display = "flex";
+PreorderContainer.style.display = "none";
+}
