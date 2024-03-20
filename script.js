81fadb17b35f91b1c8a2e565b3ee080bd14ef4d7
Yehor Kryvoi <126288081+TurboD1reX@users.noreply.github.com>
2024-03-20 08:07:17 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-
-var buttonDownNews = document.getElementById('buttonDownNews');
-const aktualne = document.querySelector('.aktualne')
-
-buttonDownNews =  function() {
-  aktualne.scrollIntoView({ behavior: 'smooth' });
-};
-
 let scrollBtn = document.getElementById("scrollBtn");
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 220 || document.documentElement.scrollTop > 220) {
     scrollBtn.style.display = "block";
   } else {
-
     scrollBtn.style.display = "none";
   }
 }
@@ -50,23 +41,23 @@ function incShopBar(){
 
 
 
-var amountElement = document.querySelector(".amount"); 
 var currentValue = parseInt(amountElement.textContent);
-var Cena = document.querySelector(".product-price");
+var Cena = document.querySelector(".cost-basket");
+var amountElement = document.querySelector(".amount-basket"); 
 var currentCena = parseFloat(Cena.textContent);  
 
-function addAmount() {
+function addAmount(cena) {
 currentValue++;
 amountElement.textContent = currentValue;
-currentCena = currentCena+19.99;
+currentCena = currentCena+cena;
 Cena.textContent = currentCena.toFixed(2);
 
 }
-function MinAmount() {
+function MinAmount(cena) {
 if (currentValue > 1) {
 currentValue --;
 amountElement.textContent = currentValue;
-currentCena = currentCena-19.99;
+currentCena = currentCena-cena;
 Cena.textContent = currentCena.toFixed(2);
 }
 }
@@ -94,7 +85,34 @@ let titleNews = document.querySelector('.title');
     }
   }
 
+  function removeProduct(title){
+   var body = document.querySelector('body');
+   const removeDiv1 = document.createElement("div");
+   removeDiv1.className = "background-product-remove";
+   removeDiv1.insertBefore(removeDiv1, body);
+   removeDiv1.innerHTML = `<div class="col-sm-11 col-md-4">
+                           <span id="delete-text">Czy napewno chcesz usunac ${title} z koszyka?</span>
+                           <div style="display:flex;justify-content;align-items;">
+                           <button id="buttonTemp buttonRemove"style="background-color:red; color:white;">Usunac</button>
+                           <button id="buttonTemp">Powrot</button>
+                           </div>
+  </div>`;
+  }
+ function addProduct(img,title,cost){
 
+  var basket = document.querySelector('.product-shop-basket');
+  basket.innerHTML += `<div class="product-card-basket col-sm-12 col-md-6">
+    <div class="img-basket"><img src="photos/test/${img}"alt=""></div>
+    <div class="delete-button-basket" onclick="removeProduct(${title})"><i class="fa fa-trash-o" style="font-size:36px"></i></div>
+    <div class="title-basket-product">${title}</div>
+    
+    <div style="display:flex;justify-content:center;align-items:center;width:fit-content;height:35px;">
+    <div class="cost-basket">${cost}</div>
+<button class="addDec-basket">+</button>
+<span class="amount-basket">1</span>
+<button class="addDec-basket">−</button>
+                </div>`
+ }
 
 
   
